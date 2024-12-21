f5c1c7019616f5eb251d3f233f1581090344ce84
maximsulim <cool.sulim@list.ru>
2024-12-21 14:50:01 +0300

    Initial Commit

---
+++ YoloModel/YoloModel.h
@@ -0,0 +1,18 @@
+//
+//  YoloModel.h
+//  YoloModel
+//
+//  Created by Максим Сулим on 21.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YoloModel.
+FOUNDATION_EXPORT double YoloModelVersionNumber;
+
+//! Project version string for YoloModel.
+FOUNDATION_EXPORT const unsigned char YoloModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoloModel/PublicHeader.h>
+
+
